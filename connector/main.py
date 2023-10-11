1f71872f3608e8fb0de1f758150ec0da36546db3
root <romandorus@gmail.com>
2023-10-11 15:47:23 +0300

    main script v0.1

---
+++ connector/main.py
@@ -0,0 +1,35 @@
+import multiprocessing
+from webs import BinanceWSS
+from https import BinanceHTTPS
+import asyncio
+
+SYMBOLS = [
+    "btcusdt@kline_1s", 
+    "ethusdt@kline_1s", 
+    "ethusdt@aggTrade", 
+    "btcusdt@aggTrade",
+    "btcusdt@orderBook", 
+    "ethusdt@orderBook"
+]
+
+async def start(connector):
+    connector.start()
+
+if __name__ == "__main__":
+    wss = BinanceWSS(
+        [n for n in SYMBOLS if n.split("@")[1] != "orderBook"]
+    )
+    https = BinanceHTTPS(
+        [n for n in SYMBOLS if n.split("@")[1] == "orderBook"]
+    )
+    
+    t1 = multiprocessing.Process(target=asyncio.run, args=(start(wss),))
+    t2 = multiprocessing.Process(target=asyncio.run, args=(start(wss),))
+    
+    t1.start()
+    t2.start()
+    
+    t1.join()
+    t2.join()
+    
+    print("Processes joined successfully")
